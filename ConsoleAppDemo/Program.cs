8b880f624ee56cf98b62abb1e653d29ffdc861e4
jaymail1995 <65511766+jaymail1995@users.noreply.github.com>
2021-03-02 10:33:59 -0500

    Added line to print

--- ConsoleAppDemo/Program.cs
+++ ConsoleAppDemo/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleAppDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is the first line!");
         }
     }
 }
